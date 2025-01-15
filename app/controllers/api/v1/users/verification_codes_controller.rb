dd3509fb3824421c23e159edf390e98c0225d741
郭晓奕 <107992047@qq.com>
2025-01-15 13:14:11 +0800

    延长token过期时间

--- app/controllers/api/v1/users/verification_codes_controller.rb
+++ app/controllers/api/v1/users/verification_codes_controller.rb
@@ -5,7 +5,7 @@ module Api
       def create
         verification_code = VerificationCode.find_or_initialize_by(phone: params[:phone])
         verification_code.save
-        result = verification_code.send_sms
+        result = verification_code.send_sms if Rails.env.production?
 
         Rails.logger.info result
         # 在此处调用Twilio或其他服务发送验证码
